53f4e92ee26a044e2fcf3124f0a9b448eb0c2ccf
Suyesh Bhandari <suyeshb@gmail.com>
2016-07-15 12:27:41 -0400

    .

--- lib/blaze_rails/engine.rb
+++ lib/blaze_rails/engine.rb
@@ -1,13 +1,13 @@
-module BlazeRails
+module Blaze
   module Rails
     class Engine < ::Rails::Engine
-      initializer 'blaze_rails-gem.assests.precompile' do |app|
+      initializer 'blaze_rails.assests.precompile' do |app|
         %w(stylesheets).each do |sub|
           app.config.assets.paths << root.join('assets', sub).to_s
         end
 
         unless Sprockets::Rails::VERSION.split('.', 2)[0].to_i >= 3
-          app.config.assets.precompile 
+          app.config.assets.precompile
         end
       end
     end
